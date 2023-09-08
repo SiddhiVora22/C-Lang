c5b3b88aee99ccfcbcd8fd324704193755d9f6e5
Siddhi Vora <144029349+SiddhiVora22@users.noreply.github.com>
2023-09-08 10:42:52 +0530

    Add files via upload

---
+++ 4(1)(4).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void  main()
+{
+  float a, b, x;
+  printf("Enter value of a: ");
+  scanf("%f", &a);
+  printf("Enter value of b: ");
+  scanf("%f", &b);
+  x = (a * a) + (b * b) + (2 * a * b);
+  printf("Result = %f\n", x);
+  printf("End of code\n");
+  
+}
