5abfcdb5dfdf391648b317511b39ff54623dbe57
Siddhi Vora <144029349+SiddhiVora22@users.noreply.github.com>
2023-09-12 20:00:25 +0530

    Add files via upload

---
+++ 5(2)(1).c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void main()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("enter a value of first number:-");
+    scanf("%d",&a);
+    printf("enter a value of second number:-");
+    scanf("%d",&b);
+    printf("enter a value of third number:");
+    scanf("%d",&c);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("A is min...");
+        }
+        else
+        {
+            printf("C is min...");
+        }
+        }
+        else
+        {
+            if(b>c)
+            {
+                printf("B is min...");
+            }
+            else
+            {
+                    printf("C is min...");
+            }
+        }
+        
+    }
